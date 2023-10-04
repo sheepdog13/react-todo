ab3c3427b170cefd62ba3d009a120da062187fb7
sheepdog13 <mikecho1234@naver.com>
2023-10-04 19:55:12 +0900

    react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
+  /* const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onchange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
+    setToDoError("");
     setToDo(value);
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(toDo);
-  };
+    if (toDo.length < 10) {
+      return setToDoError("10자 이상 작성해야 합니다");
+    }
+  }; */
+  const { register, watch } = useForm();
+
   return (
     <>
-      <form onSubmit={onSubmit}>
+      {/* <form onSubmit={onSubmit}>
         <input onChange={onchange} value={toDo} placeholder="할일" />
+        <button>추가</button>
+        {toDoError !== "" ? toDoError : null}
+      </form> */}
+      <form>
+        <input {...register("Email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("username")} placeholder="Username" />
+        <input {...register("password")} placeholder="Password" />
+        <input {...register("password1")} placeholder="Password1" />
+
         <button>추가</button>
       </form>
     </>
