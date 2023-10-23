8a809416e3de9be9b1eee4a76d3500565a3c9e1e
sheepdog13 <mikecho1234@naver.com>
2023-10-23 18:29:53 +0900

    todolist home css 수정

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -3,6 +3,25 @@ import { toDoState, toDoSelector, categoryState, Categories } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 import React from "react";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #e7c6eb;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  padding: 20px;
+  font-size: 40px;
+  font-weight: 400;
+  margin-top: 100px;
+  width: 100%;
+  border-bottom: 1px solid #ffffff;
+`;
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoSelector);
@@ -11,8 +30,8 @@ function ToDoList() {
     setCategory(event.currentTarget.value as any);
   };
   return (
-    <>
-      <h1>오늘 할일</h1>
+    <Wrap>
+      <Title>ToDoList</Title>
       <select value={category} onInput={onInput}>
         <option value={Categories.TO_DO}>할일</option>
         <option value={Categories.DOING}>하는중</option>
@@ -22,7 +41,7 @@ function ToDoList() {
       {toDos?.map((toDo) => (
         <ToDo key={toDo.id} {...toDo} />
       ))}
-    </>
+    </Wrap>
   );
 }
 
