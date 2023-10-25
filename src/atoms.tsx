632b897a7fc57c92ff3d899be124a02ebeab3ff9
sheepdog13 <mikecho1234@naver.com>
2023-10-25 23:28:30 +0900

    todos localstorage에 넣기

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -16,10 +16,10 @@ export const categoryState = atom<Categories>({
   default: Categories.TO_DO,
 });
 
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
+// export const toDoState = atom<IToDo[]>({
+//   key: "toDo",
+//   default: [],
+// });
 
 export const toDoSelector = selector({
   key: "toDoSeletor",
@@ -31,4 +31,27 @@ export const toDoSelector = selector({
       toDos.filter((toDo) => toDo.category === Categories.DONE),
     ];
   },
+});
+
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    const savedValue = localStorage.getItem(key);
+    // setSelf -> Callbacks to set or reset the value of the atom.
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    // onSet -> Subscribe to changes in the atom value.
+    onSet((newValue: any, _: any, isReset: boolean) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
+export const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+  effects: [localStorageEffect("toDo")],
 });
